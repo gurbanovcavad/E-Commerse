8f9489e7e0ff10f592a722cf77459af7fade22cb
gurbanovcavad <gurbanovcavad7@gmail.com>
2025-04-08 17:39:39 +0400

    Update Admin Page

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Comment, Listing, Bid
+
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['listing', 'content']
+    
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'category', 
+                    'winner', 'starting_bid', 'current_price']
+
+@admin.register(Bid)
+class BidAdmin(admin.ModelAdmin):
+    list_display = ['amount', 'bidder', 'amount']
